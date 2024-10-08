4354afb3a3ac2466346e3b4bde37a1833051168d
Ammar1999y <thesyclashers.911@gmail.com>
2024-10-08 04:07:16 +0300

    Commit message

--- App.js
+++ App.js
@@ -1,5 +1,18 @@
-import {SafeAreaView, ScrollView, StyleSheet, Text, View} from 'react-native';
+import {
+  Platform,
+  SafeAreaView,
+  ScrollView,
+  StyleSheet,
+  Text,
+  UIManager,
+  View,
+} from "react-native";
 
+if (
+  Platform.OS === "android" &&
+  UIManager.setLayoutAnimationEnabledExperimental
+)
+  UIManager.setLayoutAnimationEnabledExperimental(true);
 const App = () => (
   <SafeAreaView style={styles.container}>
     <ScrollView contentContainerStyle={styles.scrollContentContainer}>
@@ -10,72 +23,80 @@ const App = () => (
         style={[
           styles.box,
           {
-            transform: [{scale: 2}],
+            transform: [{ scale: 2 }],
           },
-        ]}>
+        ]}
+      >
         <Text style={styles.text}>Scale by 2</Text>
       </View>
       <View
         style={[
           styles.box,
           {
-            transform: [{scaleX: 2}],
+            transform: [{ scaleX: 2 }],
           },
-        ]}>
+        ]}
+      >
         <Text style={styles.text}>ScaleX by 2</Text>
       </View>
       <View
         style={[
           styles.box,
           {
-            transform: [{scaleY: 2}],
+            transform: [{ scaleY: 2 }],
           },
-        ]}>
+        ]}
+      >
         <Text style={styles.text}>ScaleY by 2</Text>
       </View>
       <View
         style={[
           styles.box,
           {
-            transform: [{rotate: '45deg'}],
+            transform: [{ rotate: "45deg" }],
           },
-        ]}>
+        ]}
+      >
         <Text style={styles.text}>Rotate by 45 deg</Text>
       </View>
       <View
         style={[
           styles.box,
           {
-            transform: [{rotateX: '45deg'}, {rotateZ: '45deg'}],
+            transform: [{ rotateX: "45deg" }, { rotateZ: "45deg" }],
           },
-        ]}>
+        ]}
+      >
         <Text style={styles.text}>Rotate X&Z by 45 deg</Text>
       </View>
       <View
         style={[
           styles.box,
           {
-            transform: [{rotateY: '45deg'}, {rotateZ: '45deg'}],
+            transform: [{ rotateY: "45deg" }, { rotateZ: "45deg" }],
           },
-        ]}>
+        ]}
+      >
         <Text style={styles.text}>Rotate Y&Z by 45 deg</Text>
       </View>
       <View
         style={[
           styles.box,
           {
-            transform: [{skewY: '45deg'}],
+            transform: [{ skewY: "45deg" }],
           },
-        ]}>
+        ]}
+      >
         <Text style={styles.text}>SkewY by 45 deg</Text>
       </View>
       <View
         style={[
           styles.box,
           {
-            transform: [{skewX: '30deg'}, {skewY: '30deg'}],
+            transform: [{ skewX: "30deg" }, { skewY: "30deg" }],
           },
-        ]}>
+        ]}
+      >
         <Text style={styles.text}>Skew X&Y by 30 deg</Text>
       </View>
     </ScrollView>
@@ -87,7 +108,7 @@ const styles = StyleSheet.create({
     flex: 1,
   },
   scrollContentContainer: {
-    alignItems: 'center',
+    alignItems: "center",
     paddingBottom: 60,
   },
   box: {
@@ -95,16 +116,16 @@ const styles = StyleSheet.create({
     width: 100,
     borderRadius: 5,
     marginVertical: 40,
-    backgroundColor: '#61dafb',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#61dafb",
+    alignItems: "center",
+    justifyContent: "center",
   },
   text: {
     fontSize: 14,
-    fontWeight: 'bold',
+    fontWeight: "bold",
     margin: 8,
-    color: '#000',
-    textAlign: 'center',
+    color: "#000",
+    textAlign: "center",
   },
 });
 
